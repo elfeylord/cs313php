17df7051df2a76fc3f759859557472019cc59424
Cole McAllister <elfeylord@gmail.com>
2015-02-04 23:36:56 -0700

    code from online

--- dataBaseTest.php
+++ dataBaseTest.php
@@ -4,7 +4,35 @@
 	</head>
 	<body>
 		<?php
-			try
+			 $dbHost = "";
+  $dbPort = "";
+  $dbUser = "";
+  $dbPassword = "";
+
+     $dbName = "fishing";
+
+     $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+     if ($openShiftVar === null || $openShiftVar == "")
+     {
+          // Not in the openshift environment
+          //echo "Using local credentials: "; 
+          //require("setLocalDatabaseCredentials.php");
+     }
+     else 
+     { 
+          // In the openshift environment
+          //echo "Using openshift credentials: ";
+
+          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+          $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+          $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+          $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+     } 
+     //echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+
+     $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+			/*try
 			{
 			   $user = "php";
 			   $password = "php-pass"; 
@@ -15,7 +43,7 @@
 			{
 			   echo "Error!: " . $ex->getMessage();
 			   die(); 
-			}
+			}*/
 			foreach ($db->query("SELECT name, password, email FROM scriptures;") as $row)
 			{
 			   echo "<p>" . $row['password'] . " " . $row['name'] . ":" . $row['email'] . " ";
