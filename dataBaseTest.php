0572f4d05ce69b714e2f3759d360e5ad53d81ce1
Cole McAllister <elfeylord@gmail.com>
2015-02-10 19:01:00 -0700

    do stuff!

--- dataBaseTest.php
+++ dataBaseTest.php
@@ -4,7 +4,11 @@
 	</head>
 	<body>
 		<?php
-			 
+			 $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 			try
 			{
 			   $user = "php";
