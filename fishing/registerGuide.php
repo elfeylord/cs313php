e15616e8bd83f132ae31e4a6d87e002099369965
Cole McAllister <elfeylord@gmail.com>
2015-02-12 16:37:00 -0700

    Changed stuff, and added echos for testing
    testing

--- fishing/registerGuide.php
+++ fishing/registerGuide.php
@@ -12,16 +12,18 @@
 			<p>GUIDES</p>
 			<?php
 				//Get the POST variables
+				echo("<p>all is well 1</p>");
 				$username = $_POST['username'];
 				$password = $_POST['password'];
 				$information = $_POST['information'];
-				
+				echo("<p>all is well 2</p>");
 				//Get the access variables
 				$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 				$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
 				$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 				$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 				//open the DB
+				echo("<p>all is well 3</p>");
 				try
 				{
 				   //$user = "php";
@@ -33,13 +35,15 @@
 				   echo "Error!: " . $ex->getMessage();
 				   die(); 
 				}
-				
+				echo("<p>all is well 4</p>");
 				$query = 'INSERT INTO guide(information, username, password) VALUES(:information, :username, :password)';
+				echo("<p>all is well 4.2</p>");
 				$statement = $db->prepare($query);
 				$statement->bindParam(':information', $information);
 				$statement->binfParam(':username', $username);
 				$statement->bindParam(':password', $password);
 				$statement->execute();
+				echo("<p>all is well 5</p>");
 				echo("<h1>" . $username . "You have now registered </h1>");
 			?>
 		</div>
